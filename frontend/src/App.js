97fa01a8c6b2a95ba75b75da82e05d2d55e9fcd1
AyeshaNayyer <nayyerayesha21@gmail.com>
2024-05-31 10:12:44 +0500

    noor and hiba3 branch combined with updated file structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -25,13 +25,20 @@ import Services from "./pages/Services/Services.js";
 import { Provider, useSelector } from "react-redux";
 import store from "./redux/store"; // Import the Redux store
 import VendorHome from "./pages/Vendor/VendorProfile.js";
-import AdminHome from "./pages/Admin/AdminHome.js";
+import AdminHome from "./pages/Admin/AdminHome.js"; 
 import Venues from "./pages/Services/Venues.js"; // Import your Venues component
 import Caterings from "./pages/Services/Catering.js";
 import Photography from "./pages/Services/Photography.js";
 import Decor from "./pages/Services/Decor.js";
 //import Services1 from "./pages/Services/Services.js";
 import ServiceDetail from "./pages/Services/ServiceDetail.js";
+import VendorDashboardPage from "./pages/Vendor/VendorDashboardPage.js";
+import VendorProfile from "./pages/Vendor/VendorProfile.js";
+import VendorServices from "./pages/Vendor/VendorServices.js";
+import VendorBookingHistory from "./pages/Vendor/VendorBookingHistory.js";
+import VendorPendingBookings from "./pages/Vendor/VendorPendingBookings.js";
+import VendorConfirmedBookings from "./pages/Vendor/VendorConfirmedBookings.js";
+import VendorCancelledBookings from "./pages/Vendor/VendorCancelledBookings.js";
 
 function App() {
   const { loggedIn, role } = useSelector((state) => state.user);
@@ -43,29 +50,38 @@ function App() {
         <div className="App">
           <Routes>
             <Route path="/" element={<Home />} /> {/* Home page route */}
-            <Route path="/venues" element={<Venues />} />{" "}
-            {/* Venues page route */}
-            <Route path="/catering" element={<Caterings />} />{" "}
-            {/* Venues page route */}
-            <Route path="/photography" element={<Photography />} />{" "}
-            {/* Venues page route */}
-            <Route path="/decor" element={<Decor />} />{" "}
-            {/* Venues page route */}
-           
-          
-            <Route path="/servicedetail" component={<ServiceDetail />} />
-            <Route path="/service/:id" element={<ServiceDetail />} />
+            <Route path="/venues" element={<Venues />} /> {/* Venues page route */}
+            <Route path="/catering" element={<Caterings />} /> {/* Venues page route */}
+            <Route path="/photography" element={<Photography />} /> {/* Venues page route */}
+            <Route path="/decor" element={<Decor />} /> {/* Venues page route */}
             <Route path="/services" element={<Services />} />
-            <Route path="/signup" element={<SignUp />} />
-            <Route path="/login" element={<Login />} />
-            {/* Vendor routes */}
-            {loggedIn && role === "vendor" && (
-              <Route path="/vendorhome" element={<VendorHome />} />
-            )}
-            {/* Admin routes */}
-            {loggedIn && role === "admin" && (
-              <Route path="/adminhome" element={<AdminHome />} />
-            )}
+            <Route path="/servicedetail" component={<ServiceDetail />} />
+            <Route path="/service/:id" element={<ServiceDetail />} />  
+            <Route path="/signup" element={<SignUp />} /> 
+            <Route path="/login" element={<Login />} /> 
+
+
+      {/* Vendor routes */}
+            <Route path="/vendordashboard" element={<VendorDashboardPage />} />
+            <Route path="/vendorprofile" element={<VendorProfile />} />
+            <Route path="/vendorservices" element={<VendorServices />} />
+            <Route path="/allbookings" element={<VendorBookingHistory />} />
+            <Route path="/pendingbookings" element={<VendorPendingBookings />} />
+            <Route path="/confirmedbookings" element={<VendorConfirmedBookings />} />
+            <Route path="/cancelledbookings" element={<VendorCancelledBookings />} />
+
+      {/* {loggedIn && role === 'vendor' && (
+        <Route path="/vendordashboard" element={<VendorDashboardPage />} />
+      )} */}
+       
+
+      {/* Admin routes */}
+      {loggedIn && role === 'admin' && (
+        <Route path="/adminhome" element={<AdminHome />} />
+      )}
+
+     
+            
             {/* Add other routes as needed */}
           </Routes>
         </div>
@@ -75,3 +91,5 @@ function App() {
 }
 
 export default App;
+
+
